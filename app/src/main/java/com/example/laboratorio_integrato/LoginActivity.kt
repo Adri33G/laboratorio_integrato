22279b9aac843d39cb008087d849c8a7e7ae04fe
NicoFrig <nicolo.frighi@edu.itspiemonte.it>
2022-12-09 11:39:30 +0100

    Prova del login

--- app/src/main/java/com/example/laboratorio_integrato/LoginActivity.kt
+++ app/src/main/java/com/example/laboratorio_integrato/LoginActivity.kt
@@ -3,21 +3,43 @@ package com.example.laboratorio_integrato
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity (){
 
     private lateinit var loginButton: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email:EditText
+    private lateinit var pwd:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+        email = findViewById(R.id.email)
+        pwd = findViewById(R.id.password)
         loginButton = findViewById(R.id.buttonLogin);
         loginButton.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent);
-            finish();
+            Log.d("email", email.text.toString())
+            Log.d("password", pwd.text.toString())
+            auth.signInWithEmailAndPassword(email.text.toString(), pwd.text.toString())
+                .addOnSuccessListener {
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent);
+                    finish();
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Email o Password errate", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
